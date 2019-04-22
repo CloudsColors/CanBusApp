9677a301a3622ecb4c58a99fbe0865ab5bead0ee
cloudscolors <andreas.mansson@live.com>
2019-04-22 16:54:44 +0200

    made changes to CanBusApp.java

--- src/main/java/Main/CanBusApp.java
+++ src/main/java/Main/CanBusApp.java
@@ -6,7 +6,6 @@ import CanWrapper.CanlibException;
 import CanWrapper.Handle;
 import CanWrapper.Message;
 import CanWrapper.NoMessageException;
-import Main.CAN_CODES;
 
 public class CanBusApp {
 
@@ -21,34 +20,52 @@ public class CanBusApp {
         handle.setBusParams(Canlib.canBITRATE_500K, 0, 0, 0, 0, 0);
     }
 
-    public int getRPM() throws CanlibException{
+    public int getRequested(String request) throws CanlibException{
+        switch(request){
+            case("RPM"):{
+                Message receivedMsg = getFromCan(CAN_CODES.ENGINE_RPM);
+                int dataA = receivedMsg.data[3]; // DataA according to byte[] structure is byte 4
+                int dataB = receivedMsg.data[4]; // DataB according to byte[] structure is byte 5
+                int RPM = (256*dataA+dataB)/4; // Formula to compute RPM from byte data.
+                return RPM;
+            }
+            case("TH-PS"):{
+                Message receivedMsg = getFromCan(CAN_CODES.THROTTLE_POSITION);
+                int dataA = receivedMsg.data[3]; // DataA according to byte[] structure is byte 4
+                int Throttle_position = (100/255)*dataA; // Formula to compute throttle position from byte data.
+                return Throttle_position;
+            }
+            default:{
+                return -1;
+            }
+        }
+    }
+
+    private Message getFromCan(int requestMsg) throws CanlibException{
         // The handle goes on the bus
         handle.busOn();
         //send a request for ENGINE_RPM
-        handle.write(new Message(CAN_CODES.REQUEST_MSG, new byte[]{0x2, CAN_CODES.MODE_SCD, CAN_CODES.ENGINE_RPM, 0x55, 0x55, 0x55, 0x55}, 8, 0));
+        handle.write(new Message(CAN_CODES.REQUEST_MSG, new byte[]{0x2, CAN_CODES.MODE_SCD, (byte) requestMsg, 0x55, 0x55, 0x55, 0x55}, 8, 0));
         // Wait for a message at most 1s.
         if(handle.hasMessage(1000)){
             // if there's a message, read the message.
             Message msg = handle.read();
             // Check if received data is RPM related.
-            if(msg.data[2] == CAN_CODES.ENGINE_RPM){
-                int dataA = msg.data[3]; // DataA according to byte[] structure is byte 4
-                int dataB = msg.data[4]; // DataB according to byte[] structure is byte 5
-                int RPM = (256*dataA+dataB)/4; // Formula to compute RPM from byte data.
+            if(msg.data[2] == requestMsg){
                 handle.busOff();
-                return RPM;
+                return msg;
             }else{
                 if(msg.isErrorFrame()) {
                     //if msg is an error
                     handle.busOff();
-                    return -1;
+                    return null;
                 }
             }
         }else{
             //if no message, either vehicle turned off or some sort of error.
             handle.busOff();
-            return -1;
+            return null;
         }
-        return -1;
+        return null;
     }
 }
